3e9e6f4e203f1f0f8179873b7a4cd73fbc187573
Arns <karns24@hotmail.com>
2013-08-15 20:42:28 -0500

    Initial project version

---
+++ practice.php
@@ -0,0 +1,22 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title>Practice</title>
+</head>
+<body>
+	<div id="mainContent">
+		<div id="col1">
+			<img src="Desert.jpg" alt="Desert.jpg" />
+			The desert is a ver warm place. Known for being arid, the plants have adapted to minimum water usage. 
+			The desert is a ver warm place. Known for being arid, the plants have adapted to minimum water usage.
+			The desert is a ver warm place. Known for being arid, the plants have adapted to minimum water usage.
+		</div>
+		<div id="col2">
+			<img src="Tulips.jpg" alt="Tulips.jpg" />
+		</div>
+	</div>
+	<footer>
+		<p>Practice &copy;2013 - ArnsBros.com <a href="practice.php">Contact Us</a></p>
+	</footer>
+</body>
+</html>
